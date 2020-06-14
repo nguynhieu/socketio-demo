b72837948207d7a0c984eae430705e8b35ec9749
Nguyen Hieu <tthzicc@gmail.com>
2020-06-14 21:52:28 +0700

    Add template engines

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ const app = express()
 const port = 3000
 const server = require('http').Server(app);
 
+app.set('view engine', 'pug')
+app.set('views', './views')
+
 app.use(express.static('public'))
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.render('index'))
 
 server.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
