de6db4ed18efb202163c28ee0c65aa8cf1b8749b
Nguyen Hieu <tthzicc@gmail.com>
2020-06-16 09:55:04 +0700

    Add register and logout user function

--- index.js
+++ index.js
@@ -4,10 +4,28 @@ const port = 3000
 const server = require('http').Server(app);
 const io = require('socket.io')(server)
 
+const usersOnline = [];
+
 io.on("connection", socket => {
-  console.log("connected: ", socket.id);
+
+  console.log(socket.id, " connected...");
   socket.on("disconnect", () => {
-    console.log("disconected...")
+    console.log(socket.id, " disconected...")
+  })
+  socket.on("user-send-registered", (data) => {
+    if (usersOnline.includes(data)) {
+      socket.emit("server-send-failed-register")
+    } else {
+      socket.username = data;
+      usersOnline.push(data);
+      socket.emit("server-send-success-register", data)
+      io.sockets.emit("server-send-list-users", usersOnline)
+    }
+  })
+  socket.on("user-send-logout", data => {
+    const indexUser = usersOnline.indexOf(data);
+    usersOnline.splice(indexUser, 1);
+    io.sockets.emit("server-send-list-users", usersOnline)
   })
 })
 
--- public/script.js
+++ public/script.js
@@ -1,4 +1,35 @@
 const socket = io('http://localhost:3000');
 
+socket.on("server-send-failed-register", data => {
+  alert('Failed register, user already exists');
+})
+
+socket.on("server-send-success-register", data => {
+  $("#currentUser").html(data);
+  $('#loginForm').hide(1000);
+  $('#chatForm').show(500);
+
+  $('#btnLogout').click(() => {
+    socket.emit("user-send-logout", data);
+    $('#loginForm').show(1000);
+    $('#chatForm').hide(500);
+  })
+})
+
+socket.on("server-send-list-users", data => {
+  $("#user-online").html("");
+  data.forEach(element => {
+    $("#user-online").append(`<div class='user'>${element}</div>`)
+  });
+})
+
 $(document).ready(function () {
+  $('#loginForm').show();
+  $('#chatForm').hide();
+
+  $('#btnRegister').click(() => {
+    let value = $('#txtUsername').val();
+    socket.emit("user-send-registered", value)
+  })
+
 });
